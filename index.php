c821d181bddf85e064a0199088d88bb1a72bbcbc
Stephan Weinhold <stephan.weinhold@gmail.com>
2015-11-10 15:01:45 +0100

    Create index.php
    Initial commit.

---
+++ index.php
@@ -0,0 +1,23 @@
+<html>
+<head>
+<title>pimcore object listener</title>
+</head>
+<body>
+    <main>
+        <section id="object-container">
+            <?php
+            foreach ($this->objects as $object) {
+                ?>
+                <section class="object" 
+                         data-oid="<?= $object->getId(); ?>" 
+                         data-latest-version="<?= $object->getVersions()[0]->getDate(); ?>"
+                >
+                <?php
+            }
+            ?>
+        </section>
+    </main>
+    
+    <script type="text/javascript" src="//code.jquery.com/jquery-1.11.3.min.js">
+    <script type="text/javascript" src="https://raw.githubusercontent.com/StephanWeinhold/pimcore-object-listener/master/ping.js">
+</body>
